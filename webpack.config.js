a627aad5c8651bbfb8894340e337e7d2c0fc13f7
Sage911 <stefan.age@outlook.com>
2017-08-17 21:23:45 -0500

    Installing common Node dependencies
    Webpack, devserver for hot module reloading, Babel to write es6, and
    Babel packages that allow it to be used with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+
+  plugins: [
+
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+
+  ],
+
+  resolve: {
+    modules: ['node_modules', 'src']
+  },
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  devServer: {
+    hot: true, // Tell the dev-server we're using HMR
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+}
